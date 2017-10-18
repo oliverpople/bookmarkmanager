1261fe8f8176906da67eac2c49a781042f16a0f0
Aram Simonian <aram.contacts@gmail.com>
2017-10-18 15:29:52 +0100

    Added database_cleaner reference and associated config details

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'simplecov'
 require 'simplecov-console'
 require './app/app'
 require_relative './features/web_helper'
+require 'database_cleaner'
 
 Capybara.app = BookmarkManager
 
@@ -60,6 +61,30 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # config.use_transactional_fixtures = true
+
+  ###########################
+  # This database cleaner section was added after reading here:
+  # https://github.com/DatabaseCleaner/database_cleaner
+  # and then implementing the walkthrough config found here:
+  # https://github.com/makersacademy/course/blob/master/bookmark_manager/walkthroughs/12.md
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+# end of additional database cleaner section
+###########################
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
