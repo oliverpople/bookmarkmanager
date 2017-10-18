f64f8f1a1609efa26eba895707b0849898ea547a
Aram Simonian <aram.contacts@gmail.com>
2017-10-18 16:43:28 +0100

    Modified heroku DB url to DATABASE_URL

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,7 +11,7 @@ class Link
 end
 
 
-DataMapper.setup(:default, ENV['https://immense-woodland-99203.herokuapp.com/'] ||
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||
       "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
